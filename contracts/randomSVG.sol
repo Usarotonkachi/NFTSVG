4da787fce9096986a73ceb7c0a97d28d07ad5d72
Bamba <92602163+Usarotonkachi@users.noreply.github.com>
2021-11-21 15:02:28 +0200

    Time2

--- contracts/randomSVG.sol
+++ contracts/randomSVG.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
@@ -47,7 +47,7 @@ contract RandomSVG is ERC721URIStorage, VRFConsumerBase, Ownable {
         pathCommands = ["M", "L"];
         colors = ["red", "blue", "green", "yellow", "black", "white"];
     }
-    
+
     function withdraw() public payable onlyOwner {
         payable(owner()).transfer(address(this).balance);
     }
